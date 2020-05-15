ae5bcddb4ad0a0b1e7bf4f6e7f8f46e9f3e0915a
wekesa <vicweks65@gmail.com>
2020-05-16 00:37:02 +0300

    Optimized solution based on time complexity

--- max_loot_value.py
+++ max_loot_value.py
@@ -1,35 +1,33 @@
-# Returns the maximum value that can be put in a knapsack of
-
-items = [
-{ "weight": 5, "value": 10 },
-{ "weight": 4, "value": 40 },
-{ "weight": 6, "value": 30 },
-{ "weight": 4, "value": 50 }
-]
+# A Dynamic Programming based Python Program
+# Returns the maximum value that can be put in a knapsack of capacity W
 
 
-def knap_sack(weight_limit, items, n):
-    # Base Case
-    if n == 0 or weight_limit == 0:
-        return 0
+# An optimized solution that has a time complexity of O(N*W)
+def knap_sack(weight_limit, items, number_of_items):
+    K = [[0 for x in range(weight_limit + 1)] for x in range(number_of_items + 1)]
 
-    # If weight of the nth item is more than Knapsack of capacity
-    # W, then this item cannot be included in the optimal solution
-    if items[n - 1]["weight"] > weight_limit:
-        return knap_sack(weight_limit, items, n - 1)
+    # Build table K[][] in bottom up manner
+    for item in range(number_of_items + 1):
+        for weight in range(weight_limit + 1):
+            if item == 0 or weight == 0:
+                K[item][weight] = 0
+            elif items[item - 1]["weight"] <= weight:
+                K[item][weight] = max(items[item - 1]["value"] +
+                                      K[item - 1][weight - items[item - 1]["weight"]], K[item - 1][weight])
+            else:
+                K[item][weight] = K[item - 1][weight]
 
-    # return the maximum of two cases:
-    # (1) nth item included
-    # (2) not included
-    else:
-        return max(items[n - 1]["value"] + knap_sack(weight_limit - items[n - 1]["weight"], items, n - 1),
-                   knap_sack(weight_limit, items, n - 1))
-
-    # end of function knapSack
+    return K[number_of_items][weight_limit]
 
 
 # To test above function
 if __name__ == '__main__':
+    sample_items = [
+        {"weight": 5, "value": 10},
+        {"weight": 4, "value": 40},
+        {"weight": 6, "value": 30},
+        {"weight": 4, "value": 50}
+    ]
     knap_sack_limit = 10
-    items_length = len(items)
-    print(knap_sack(knap_sack_limit, items, items_length))
+    items_length = len(sample_items)
+    print(knap_sack(knap_sack_limit, sample_items, items_length))
